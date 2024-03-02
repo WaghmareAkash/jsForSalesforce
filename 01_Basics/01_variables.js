0f48a7b5e631cecb0282ea1cab93013757063e2e
WaghmareAkash <106730323+WaghmareAkash@users.noreply.github.com>
2024-03-02 07:18:00 +0000

    variable commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 21353
+let accountEmail = "abc@abc.com"
+var accountName = "ABC"
+accountCity = "Pune"
+// do not reassign value to const as its value can not be changed
+/*
+use let instead of var,var should not be used.
+because it cn not identify the scope 
+*/
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountName, accountCity]);
